5e36efff28a2f4fa133c3506e77142ec3fab5383
zhouxiaohua <zhouxiaohua@cnic.cn>
2017-07-23 13:45:00 +0800

    Part III: Distributing MapReduce tasks

--- main/main.go
+++ main/main.go
@@ -2,9 +2,37 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func main()  {
-	var x []string
-	fmt.Println(x==nil)
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	ch1 := make(chan string, 3)
+
+	ch1 <- "a"
+	ch1 <- "b"
+	ch1 <- "c"
+
+	go func() {
+		for {
+			fmt.Println("-----------")
+
+			x := <- ch1
+
+			go func() {
+				fmt.Println(x)
+				wg.Done()
+			}()
+
+		}
+	}()
+
+	fmt.Println("xxxxxxxxxxxxx")
+
+	wg.Wait()
+
+	fmt.Println("all is over")
+
 }
--- mapreduce/schedule.go
+++ mapreduce/schedule.go
@@ -1,6 +1,12 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"sync"
+	"log"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (Map
@@ -14,23 +20,77 @@ import "fmt"
 func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, registerChan chan string) {
 	var ntasks int
 	var n_other int // number of inputs (for reduce) or outputs (for map)
+	var inputArgs chan string
 	switch phase {
 	case mapPhase:
 		ntasks = len(mapFiles)
 		n_other = nReduce
+		inputArgs = make(chan string, ntasks)
+		for idx, mf := range (mapFiles) {
+			arg := strconv.Itoa(idx) + " " + mf
+			inputArgs <- arg
+		}
 	case reducePhase:
 		ntasks = nReduce
 		n_other = len(mapFiles)
+		inputArgs = make(chan string, ntasks)
+		for idx := 0; idx < ntasks; idx++ {
+			arg := strconv.Itoa(idx)
+			inputArgs <- arg
+		}
 	}
 
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
+
+	var wg sync.WaitGroup
+	wg.Add(ntasks)
+
+	go func() {
+		for {
+
+			wk := <-registerChan
+
+			go func() {
+				input_arg := <-inputArgs
+				args := strings.Fields(input_arg)
+				taskNum, err := strconv.Atoi(args[0])
+				if err != nil {
+					log.Fatalf("Number(%s) parse error: %s\n", args[0], err)
+				}
+
+				taskArg := &DoTaskArgs{
+					JobName:       jobName,
+					Phase:         phase,
+					TaskNumber:    taskNum,
+					NumOtherPhase: n_other,
+				}
+
+				if phase == mapPhase {
+					taskArg.File = args[1]
+				}
+
+				ok := call(wk, "Worker.DoTask", &taskArg, new(struct{}))
+
+				defer func() {
+					registerChan <- wk
+				}()
+
+				if !ok {
+					inputArgs <- input_arg
+				} else {
+					wg.Done()
+				}
+
+			}()
+		}
+	}()
+
+	wg.Wait()
 
 	// All ntasks tasks have to be scheduled on workers, and only once all of
 	// them have been completed successfully should the function return.
 	// Remember that workers may fail, and that any given worker may finish
 	// multiple tasks.
-	//
-	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
